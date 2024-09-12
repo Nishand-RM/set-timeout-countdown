d24f2805c3ae10ba797155e8c5ec753d18c165c0
Nishand-RM <nishandrm9@gmail.com>
2024-09-12 12:57:21 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function countdown() {
-    const output = document.getElementById('output');
-    output.innerHTML = ''; 
+document.getElementById('output').innerHTML = '10';
 
-    for (let i = 1; i <= 10; i++) {
-        setTimeout(() => {
-            output.innerHTML = i; 
-        }, i * 1000); 
-    }
-
-    setTimeout(() => {
-        output.innerHTML = 'Happy Birthday!'; 
-    }, 11 * 1000); 
-}
-
-countdown();
+setTimeout(function () {
+    document.getElementById('output').innerHTML = '9';
+    setTimeout(function () {
+        document.getElementById('output').innerHTML = '8';
+        setTimeout(function () {
+            document.getElementById('output').innerHTML = '7';
+            setTimeout(function () {
+                document.getElementById('output').innerHTML = '6';
+                setTimeout(function () {
+                    document.getElementById('output').innerHTML = '5';
+                    setTimeout(function () {
+                        document.getElementById('output').innerHTML = '4';
+                        setTimeout(function () {
+                            document.getElementById('output').innerHTML = '3';
+                            setTimeout(function () {
+                                document.getElementById('output').innerHTML = '2';
+                                setTimeout(function () {
+                                    document.getElementById('output').innerHTML = '1';
+                                    setTimeout(function () {
+                                        document.getElementById('output').innerHTML = 'Happy Independence!';
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
